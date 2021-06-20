182e392119b9a23f7bf79a8a3fe3358a8fa8d475
e9x <x@sys32.dev>
2021-06-20 17:10:18 -0400

    idkr scripts

--- junker.idkr.js
+++ junker.idkr.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-    name: 'Skidfest',
+    name: 'Junker',
     author: 'The Gaming Gurus',
     locations: [ 'game' ],
     run(){
 		var request = new XMLHttpRequest();
-		request.open('GET', 'https://y9x.github.io/userscripts/skidfest.user.js', false);
+		request.open('GET', 'https://y9x.github.io/userscripts/junker.user.js', false);
 		request.send();
 		new Function('LICENSE_KEY', request.responseText)('idkr');
     },
